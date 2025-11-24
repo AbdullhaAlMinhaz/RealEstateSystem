ff5185901fabcff360456797512c2643fabe3060
Abdullha Al Minhaz <97035413+AbdullhaAlMinhaz@users.noreply.github.com>
2025-11-24 15:51:33 +0600

    Admin - Design

---
+++ RealEstateSystem/Controllers/AdminProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateSystem.Controllers
+{
+    public class AdminProfileController : Controller
+    {
+        // later we can load the real admin data from DB / session
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstateSystem/Controllers/AdminReportsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateSystem.Controllers
+{
+    public class AdminReportsController : Controller
+    {
+        // GET: /AdminReports
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
