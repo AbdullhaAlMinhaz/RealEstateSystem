390cd5cee71759bb4a67d5817ea754d910e34f24
Abdullha Al Minhaz <97035413+AbdullhaAlMinhaz@users.noreply.github.com>
2025-12-21 12:10:46 +0600

    Done the message option for all..

---
+++ RealEstateSystem/Controllers/AdminMessagesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using RealEstateSystem.Models;
+
+namespace RealEstateSystem.Controllers
+{
+    public class AdminMessagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            var roleStr = HttpContext.Session.GetString("UserRole");
+
+            if (userId == null) return RedirectToAction("Login", "Account");
+
+            // UserRole is stored as int string: "1" for Admin
+            if (string.IsNullOrWhiteSpace(roleStr) ||
+                roleStr != ((int)UserRole.Admin).ToString())
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+    }
+}
